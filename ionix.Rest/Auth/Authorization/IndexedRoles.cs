357659cd6294a8e681f9621dfdcd8c5f800a7d32
Mehmet Goren <mehmetgoren@outlook.com>
2017-08-15 14:46:50 +0300

    1

--- ionix.Rest/Auth/Authorization/IndexedRoles.cs
+++ ionix.Rest/Auth/Authorization/IndexedRoles.cs
@@ -4,7 +4,6 @@
     using System;
     using System.Collections.Generic;
 
-
     //Amaç RoleControllerActionEntity entityi parçalayıup indexlemek hızlı check işlemleri için.
     public sealed class IndexedRoles
     {
